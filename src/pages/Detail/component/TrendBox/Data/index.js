1ded477509020ba866c301bfc8f733f904dec5f4
zjq383688496 <zjq383688496@gmail.com>
2020-12-08 00:17:23 +0800

    _

--- src/pages/Detail/component/TrendBox/Data/index.js
+++ src/pages/Detail/component/TrendBox/Data/index.js
@@ -11,12 +11,28 @@ export default class DataBox extends React.Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			list:  [],
-			times: [],
+			list:   [],
+			times:  [],
+			height: 0,
 		}
 	}
 	componentDidMount() {
+		let { body } = this.refs
 		this.getData()
+		body.addEventListener('scroll', this.scrollUpdate.bind(this), false)
+	}
+	componentWillUnmount() {
+		let { body } = this.refs
+		body.removeEventListener('scroll', this.scrollUpdate)
+	}
+	scrollUpdate() {
+		let { times } = this.state
+		let { body, header } = this.refs
+		let sl   = body.scrollLeft
+		let idx  = parseInt(sl / 40)
+		let time = times[idx]
+		header.scrollLeft = sl
+		console.log(idx, time)
 	}
 	getData = async () => {
 		await _wait(100)
@@ -30,7 +46,8 @@ export default class DataBox extends React.Component {
 			})
 		}
 		let list = new Array(12).fill().map(_ => item)
-		this.setState({ list }, this.getTimes)
+
+		this.setState({ list, height: 24 * 12 }, this.getTimes)
 	}
 	getTimes = () => {
 		let { list } = this.state
@@ -61,8 +78,8 @@ export default class DataBox extends React.Component {
 	renderTh = () => {
 		let { times } = this.state
 		let ths = [
-			<th key={0} style={{ position: 'sticky', left: 0 }}>参数</th>,
-			<th key={1} style={{ position: 'sticky', left: 60 }}>单位</th>,
+			<th key={0} className="db-td-fixed" style={{ position: 'sticky', left: 0 }}>参数</th>,
+			<th key={1} className="db-td-fixed" style={{ position: 'sticky', left: 60 }}>单位</th>,
 			...times.map(({ time }, i) => {
 				return <th key={i + 2}>{time}</th>
 			})
@@ -79,8 +96,8 @@ export default class DataBox extends React.Component {
 			let { data } = td
 			let { u: unit, n: name } = __Map__.m[td.name] || {}
 			let tds = [
-				<td key={0} style={{ position: 'sticky', left: 0 }}>{name}</td>,
-				<td key={1} style={{ position: 'sticky', left: 60 }}>{unit}</td>,
+				<td key={0} className="db-td-fixed" style={{ position: 'sticky', left: 0 }}>{name}</td>,
+				<td key={1} className="db-td-fixed" style={{ position: 'sticky', left: 60 }}>{unit}</td>,
 				...data.map(({ value }, j) => {
 					return <td key={j + 2}>{value}</td>
 				})
@@ -94,24 +111,32 @@ export default class DataBox extends React.Component {
 		) 
 	}
 	render() {
-		let { times } = this.state
-		if (!times.length) return null
+		let { times, height } = this.state
 		let colgroup = this.renderCol()
 		let thead    = this.renderTh()
 		let tbody    = this.renderTd()
+		let style = times.length? {}: { display: 'none' }
 		return (
-			<div className="data-box">
-				<div className="db-header">
-					<table>
-						{ colgroup }
-						{ thead }
-					</table>
+			<div className="data-box" style={style}>
+				<div className="data-box-header">
 				</div>
-				<div className="db-body">
-					<table>
-						{ colgroup }
-						{ tbody }
-					</table>
+				<div className="data-box-table" style={{ height: height + 24 }}>
+					<div ref="header" className="db-header">
+						<table>
+							{ colgroup }
+							{ thead }
+						</table>
+					</div>
+					<div ref="body" className="db-body" style={{ height }}>
+						<table>
+							{ colgroup }
+							{ tbody }
+						</table>
+					</div>
+					<div className="db-line-t"></div>
+					<div className="db-line-r"></div>
+					<div className="db-line-b"></div>
+					<div className="db-line-l"></div>
 				</div>
 			</div>
 		)
